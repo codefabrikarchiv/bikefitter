0d587bfb70e82df0fe6d2cfe2ee8f4316d560116
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-03-30 10:27:49 +0200

    file export

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod download;
 mod serial;
 mod dataframe;
 
+extern crate nfd;
+use nfd::Response;
+
+use xlsxwriter::Workbook;
+
 use iced::{
     button, executor, Align, Application, Button, Column, Command,
     Element, Settings, Subscription, Text, Radio, Clipboard, Row,
@@ -64,9 +69,39 @@ impl Application for Reader {
                 }
             }
             Message::Export => {
-                println!("EXPORT");
+                let result = nfd::open_save_dialog(Some("xlsx"), None).unwrap_or_else(|e| {
+                    panic!("{}", e);
+                });
+                match result {
+                    Response::Cancel => println!("User canceled"),
+                    Response::Okay(file_path) => {
+                        let fp = file_path + ".xlsx";
+                        let workbook = Workbook::new(&fp);
+                        let sheet = workbook.add_worksheet(None);
+                        match sheet {
+                            Ok(mut sheet) => {
+                                sheet.write_string(0, 0, "Nummer", None);
+                                sheet.write_string(0, 1, "x", None);
+                                sheet.write_string(0, 2, "y", None);
+
+                                let mut row = 1;
+                                for snapshot in &self.snapshots {
+                                    sheet.write_number(row, 0, row.into(), None);
+                                    sheet.write_number(row, 1, snapshot.x.into(), None);
+                                    sheet.write_number(row, 2, snapshot.y.into(), None);
+                                    row += 1;
+                                }
+                                workbook.close();
+                            }
+                            Err(e) => {
+                                eprintln!("{:?}", e);
+                            }
+                        }
+                    }
+                    Response::OkayMultiple(files) => println!("Files {:?}", files),
+                }
             }
-        }
+        };
 
         Command::none()
     }
